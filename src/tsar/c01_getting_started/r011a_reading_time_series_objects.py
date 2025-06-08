6c0302ae48be9c61940165f1dbd0ec05c3aed907
wilsonify <tom.andrew.wilson@gmail.com>
2025-06-07 20:02:55 -0400

    test_load_airpassenger_data

--- src/tsar/c01_getting_started/r011a_reading_time_series_objects.py
+++ src/tsar/c01_getting_started/r011a_reading_time_series_objects.py
@@ -1,20 +1,39 @@
+# airpassengers_analysis.py
 
-import matplotlib.pyplot as plt
-# %%
 import pandas as pd
-
-from c01_getting_started import path_to_data
-
-# %%
+import matplotlib.pyplot as plt
+from typing import Optional
 
 
-# %%
-data = pd.read_csv(
-    filepath_or_buffer=f"{path_to_data}/input/airpassengers.csv",
-    parse_dates=['Month'],
-    index_col='Month',
-    date_parser=pd.to_datetime
-)
+def load_airpassenger_data(csv_path: str) -> pd.DataFrame:
+    """Load AirPassengers dataset from a CSV file."""
+    return pd.read_csv(
+        filepath_or_buffer=csv_path,
+        parse_dates=['Month'],
+        index_col='Month',
+        date_parser=pd.to_datetime
+    )
 
-plt.plot(data, label='Passenger Count')
-plt.show()
+
+def plot_passenger_data(data: pd.DataFrame, ax: Optional[plt.Axes] = None) -> None:
+    """Plot passenger count from the given DataFrame."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    data.plot(ax=ax, label='Passenger Count')
+    ax.set_title("Monthly Air Passenger Count")
+    ax.set_ylabel("Passengers")
+    ax.set_xlabel("Date")
+    ax.legend()
+    plt.show()
+
+
+def main():
+    from c01_getting_started import path_to_data
+
+    csv_path = f"{path_to_data}/input/airpassengers.csv"
+    data = load_airpassenger_data(csv_path)
+    plot_passenger_data(data)
+
+
+if __name__ == "__main__":
+    main()
