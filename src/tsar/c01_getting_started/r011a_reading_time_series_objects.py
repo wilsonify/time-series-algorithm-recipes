ecf8e387a4858a59100d715b0e689ca33aa0cf92
wilsonify <tom.andrew.wilson@gmail.com>
2025-06-11 08:04:23 -0400

    r011a_reading_time_series_objects.py

--- src/tsar/c01_getting_started/r011a_reading_time_series_objects.py
+++ src/tsar/c01_getting_started/r011a_reading_time_series_objects.py
@@ -1,8 +1,11 @@
 # airpassengers_analysis.py
 
-import pandas as pd
-import matplotlib.pyplot as plt
 from typing import Optional
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from c01_getting_started import path_to_data
 
 
 def load_airpassenger_data(csv_path: str) -> pd.DataFrame:
@@ -15,21 +18,22 @@ def load_airpassenger_data(csv_path: str) -> pd.DataFrame:
     )
 
 
-def plot_passenger_data(data: pd.DataFrame, ax: Optional[plt.Axes] = None) -> None:
+def plot_passenger_data(data: pd.DataFrame, ax: Optional[plt.Axes] = None,show=False) -> None:
     """Plot passenger count from the given DataFrame."""
     if ax is None:
         fig, ax = plt.subplots()
+
     data.plot(ax=ax, label='Passenger Count')
     ax.set_title("Monthly Air Passenger Count")
     ax.set_ylabel("Passengers")
     ax.set_xlabel("Date")
     ax.legend()
-    plt.show()
+
+    if show:
+        plt.show()
 
 
 def main():
-    from c01_getting_started import path_to_data
-
     csv_path = f"{path_to_data}/input/airpassengers.csv"
     data = load_airpassenger_data(csv_path)
     plot_passenger_data(data)
--- tests/test_c01_getting_started/test_chapter_01.py
+++ tests/test_c01_getting_started/test_chapter_01.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from c01_getting_started import path_to_data
-from c01_getting_started.r011a_reading_time_series_objects import load_airpassenger_data
+from c01_getting_started.r011a_reading_time_series_objects import load_airpassenger_data, plot_passenger_data
 
 
 def test_load_airpassenger_data():
@@ -10,3 +11,9 @@ def test_load_airpassenger_data():
     assert list(df.columns) == ["#Passengers"]
     assert pd.Timestamp("1949-01-01") in df.index
     assert df.loc["1949-01-01", "#Passengers"] == 112
+
+
+def test_plot_passenger_data():
+    df = load_airpassenger_data(f"{path_to_data}/input/airpassengers.csv")
+    fig, ax = plt.subplots()
+    plot_passenger_data(df, ax, show=False)
