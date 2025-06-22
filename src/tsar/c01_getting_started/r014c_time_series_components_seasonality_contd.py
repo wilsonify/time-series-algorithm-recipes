c19b77a9e286a6e2a8e01b16bd165c77c24b87cf
wilsonify <tom.andrew.wilson@gmail.com>
2025-06-22 10:50:31 -0400

    r014c_time_series_components_seasonality_contd.py

--- src/tsar/c01_getting_started/r014c_time_series_components_seasonality_contd.py
+++ src/tsar/c01_getting_started/r014c_time_series_components_seasonality_contd.py
@@ -1,34 +1,29 @@
-import matplotlib.pyplot as plt
-# %%
 import pandas as pd
+from matplotlib import pyplot as plt
 
-from c01_getting_started import path_to_data
 
-# %%
-tractor_sales_data = pd.read_csv(f"{path_to_data}/input/tractor_salessales.csv")
-tractor_sales_data.head(5)
+def read_tractor_sales_data(filepath_or_buffer):
+    tractor_sales_data = pd.read_csv(filepath_or_buffer=filepath_or_buffer)
+    tractor_sales_data.head(5)
+    date_ser = pd.date_range(start='2003-01-01', freq='MS', periods=len(tractor_sales_data))
+    tractor_sales_data.rename(columns={'Number of Tractor Sold': 'Tractor-Sales'}, inplace=True)
+    tractor_sales_data.set_index(date_ser, inplace=True)
+    tractor_sales_data = tractor_sales_data.loc[:, 'Tractor-Sales']
+    return tractor_sales_data
 
-# %%
-date_ser = pd.date_range(start='2003-01-01', freq='MS', periods=len(tractor_sales_data))
 
-# %%
-tractor_sales_data.rename(columns={'Number of Tractor Sold': 'Tractor-Sales'}, inplace=True)
-tractor_sales_data.set_index(date_ser, inplace=True)
-tractor_sales_data = tractor_sales_data.loc[:, 'Tractor-Sales']
-tractor_sales_data.head(5)
+def plot_tractor_sales(tractor_sales_data, show=True):
+    tractor_sales_data.plot()
+    plt.ylabel('Tractor Sales')
+    plt.title("Tractor Sales from 2003 to 2014")
+    if show: plt.show()
 
-# %%
-tractor_sales_data.plot()
-plt.ylabel('Tractor Sales')
-plt.title("Tractor Sales from 2003 to 2014")
-plt.show()
 
-# %%
-
-one_year_ser = tractor_sales_data['2011']
-grouped_ser = one_year_ser.groupby(pd.Grouper(freq='M'))
-month_df = pd.concat([pd.DataFrame(x[1].values) for x in grouped_ser], axis=1)
-month_df = pd.DataFrame(month_df)
-month_df.columns = range(1, 13)
-month_df.boxplot()
-plt.show()
+def boxplot_tractor_sales_one_year(tractor_sales_data, show=True):
+    one_year_ser = tractor_sales_data['2011']
+    grouped_ser = one_year_ser.groupby(pd.Grouper(freq='M'))
+    month_df = pd.concat([pd.DataFrame(x[1].values) for x in grouped_ser], axis=1)
+    month_df = pd.DataFrame(month_df)
+    month_df.columns = range(1, 13)
+    month_df.boxplot()
+    if show: plt.show()
--- tests/test_c01_getting_started/test_r014c_time_series_components_seasonality_contd.py
+++ tests/test_c01_getting_started/test_r014c_time_series_components_seasonality_contd.py
@@ -1,34 +1,15 @@
-import matplotlib.pyplot as plt
 # %%
-import pandas as pd
 
 from c01_getting_started import path_to_data
+from c01_getting_started.r014c_time_series_components_seasonality_contd import (
+    read_tractor_sales_data,
+    plot_tractor_sales,
+    boxplot_tractor_sales_one_year
+)
 
-# %%
-tractor_sales_data = pd.read_csv(f"{path_to_data}/input/tractor_salessales.csv")
-tractor_sales_data.head(5)
 
-# %%
-date_ser = pd.date_range(start='2003-01-01', freq='MS', periods=len(tractor_sales_data))
-
-# %%
-tractor_sales_data.rename(columns={'Number of Tractor Sold': 'Tractor-Sales'}, inplace=True)
-tractor_sales_data.set_index(date_ser, inplace=True)
-tractor_sales_data = tractor_sales_data.loc[:, 'Tractor-Sales']
-tractor_sales_data.head(5)
-
-# %%
-tractor_sales_data.plot()
-plt.ylabel('Tractor Sales')
-plt.title("Tractor Sales from 2003 to 2014")
-plt.show()
-
-# %%
-
-one_year_ser = tractor_sales_data['2011']
-grouped_ser = one_year_ser.groupby(pd.Grouper(freq='M'))
-month_df = pd.concat([pd.DataFrame(x[1].values) for x in grouped_ser], axis=1)
-month_df = pd.DataFrame(month_df)
-month_df.columns = range(1, 13)
-month_df.boxplot()
-plt.show()
+def test_r014c_time_series_components_seasonality_contd():
+    tractor_sales_data = read_tractor_sales_data(f"{path_to_data}/input/tractor_salessales.csv")
+    assert tractor_sales_data.shape == (144,)
+    plot_tractor_sales(tractor_sales_data, show=False)
+    boxplot_tractor_sales_one_year(tractor_sales_data, show=False)
