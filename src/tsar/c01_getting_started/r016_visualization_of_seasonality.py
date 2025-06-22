5a5943524119afeb69e201c80a31d3312bb91ee1
wilsonify <tom.andrew.wilson@gmail.com>
2025-06-22 11:09:01 -0400

    r016_visualization_of_seasonality.py

--- src/tsar/c01_getting_started/r016_visualization_of_seasonality.py
+++ src/tsar/c01_getting_started/r016_visualization_of_seasonality.py
@@ -1,26 +1,23 @@
-import matplotlib.pyplot as plt
-# %%
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from c01_getting_started import path_to_data
 
-# %%
 
-# %%
-turn_over_data = pd.read_csv(f'{path_to_data}/input/retailturnover.csv')
-date_range = pd.date_range(start='1/7/1982', end='31/3/1992', freq='Q')
-turn_over_data['TimeIndex'] = pd.DataFrame(date_range, columns=['Quarter'])
+def boxplot_quarterly_turn_over_data(quarterly_turn_over_data, show=True):
+    quarterly_turn_over_data.boxplot()
+    if show: plt.show()
 
-# %%
-quarterly_turn_over_data = pd.pivot_table(turn_over_data, values="Turnover", columns="Quarter", index="Year")
-quarterly_turn_over_data
 
-# %%
-quarterly_turn_over_data.plot()
-plt.show()
+def plot_quarterly_turn_over_data(turn_over_data, show=True):
+    quarterly_turn_over_data = pd.pivot_table(turn_over_data, values="Turnover", columns="Quarter", index="Year")
+    quarterly_turn_over_data.plot()
+    if show: plt.show()
+    return quarterly_turn_over_data
 
-# %%
-quarterly_turn_over_data.boxplot()
-plt.show()
 
-# %%
+def read_turn_over_data():
+    turn_over_data = pd.read_csv(f'{path_to_data}/input/retailturnover.csv')
+    date_range = pd.date_range(start='1/7/1982', end='31/3/1992', freq='Q')
+    turn_over_data['TimeIndex'] = pd.DataFrame(date_range, columns=['Quarter'])
+    return turn_over_data
--- tests/test_c01_getting_started/test_r016_visualization_of_seasonality.py
+++ tests/test_c01_getting_started/test_r016_visualization_of_seasonality.py
@@ -1,26 +1,8 @@
-import matplotlib.pyplot as plt
-# %%
-import pandas as pd
+from c01_getting_started.r016_visualization_of_seasonality import boxplot_quarterly_turn_over_data, \
+    plot_quarterly_turn_over_data, read_turn_over_data
 
-from c01_getting_started import path_to_data
 
-# %%
-
-# %%
-turn_over_data = pd.read_csv(f'{path_to_data}/input/retailturnover.csv')
-date_range = pd.date_range(start='1/7/1982', end='31/3/1992', freq='Q')
-turn_over_data['TimeIndex'] = pd.DataFrame(date_range, columns=['Quarter'])
-
-# %%
-quarterly_turn_over_data = pd.pivot_table(turn_over_data, values="Turnover", columns="Quarter", index="Year")
-quarterly_turn_over_data
-
-# %%
-quarterly_turn_over_data.plot()
-plt.show()
-
-# %%
-quarterly_turn_over_data.boxplot()
-plt.show()
-
-# %%
+def test_r016_visualization_of_seasonality():
+    turn_over_data = read_turn_over_data()
+    quarterly_turn_over_data = plot_quarterly_turn_over_data(turn_over_data, show=False)
+    boxplot_quarterly_turn_over_data(quarterly_turn_over_data, show=False)
